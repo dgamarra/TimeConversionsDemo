232f5d83b41747de294a96013b386c9428a76cbe
Daniel Gamarra <dgamarra@continental.edu.pe>
2023-02-01 12:16:50 +0000

    Se cambia español nombre del método

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_hours_to_minutes(hours):
+def convertirHorasAMinutos(hours):
   if not isinstance(hours, (int, float)):
     return "Oops! Las horas deben ser int o float. 'horas' como una <class 'str'>"
   return 60 * hours
 
-print(convert_hours_to_minutes(30))
+print(convertirHorasAMinutos(30))
